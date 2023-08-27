76a4acea4bd47d4c7ee1350baa85287d37d4b164
Cody Sheridan <cody.sheridan@protonmail.com>
2023-08-27 08:51:14 -0400

    Selector component created.

--- components/atoms/index.ts
+++ components/atoms/index.ts
@@ -1,3 +1,4 @@
+export { default as Selector } from './selector/Selector'
 export { default as NavIcon } from './nav-icon/NavIcon'
 export { default as Select } from './select/Select'
 export { default as Title } from './title/Title'
---
+++ components/atoms/selector/Selector.test.tsx
@@ -0,0 +1,52 @@
+import Selector from './Selector'
+import { render, fireEvent } from '@testing-library/react-native'
+
+describe('Selector component', () => {
+  const testSelector = () => {
+    return render(
+      <Selector size={20} quantity={3} color='black' padding={10} />
+    )
+  }
+
+  it('renders correct number of circles', () => {
+    const { queryAllByTestId } = testSelector()
+
+    const circles = queryAllByTestId(/circle-\d+/)
+    expect(circles.length).toBe(3)
+  })
+
+  it('has only one circle selected at time', () => {
+    const { getByTestId, getAllByTestId } = testSelector()
+
+    expect(getByTestId('selected-circle-0')).toBeTruthy()
+    expect(getAllByTestId(/deselected-circle-\d+/).length).toBe(2)
+  })
+
+  it('toggles selection on press', () => {
+    const { getByTestId } = testSelector()
+
+    const circle0 = getByTestId('circle-0')
+    const circle1 = getByTestId('circle-1')
+    const circle2 = getByTestId('circle-2')
+
+    // Default state.
+    expect(getByTestId('selected-circle-0')).toBeTruthy()
+    expect(getByTestId('deselected-circle-1')).toBeTruthy()
+    expect(getByTestId('deselected-circle-2')).toBeTruthy()
+
+    fireEvent.press(circle1)
+    expect(getByTestId('deselected-circle-0')).toBeTruthy()
+    expect(getByTestId('selected-circle-1')).toBeTruthy()
+    expect(getByTestId('deselected-circle-2')).toBeTruthy()
+
+    fireEvent.press(circle2)
+    expect(getByTestId('deselected-circle-0')).toBeTruthy()
+    expect(getByTestId('deselected-circle-1')).toBeTruthy()
+    expect(getByTestId('selected-circle-2')).toBeTruthy()
+
+    fireEvent.press(circle0)
+    expect(getByTestId('selected-circle-0')).toBeTruthy()
+    expect(getByTestId('deselected-circle-1')).toBeTruthy()
+    expect(getByTestId('deselected-circle-2')).toBeTruthy()
+  })
+})
