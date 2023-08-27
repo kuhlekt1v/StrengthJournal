3b193ee4bc440f930bda9468b98a4617c4b81997
Cody Sheridan <cody.sheridan@protonmail.com>
2023-08-27 08:50:26 -0400

    Include settings screen placeholder.

--- App.tsx
+++ App.tsx
@@ -1,16 +1,17 @@
 import { View } from 'react-native'
 import WorkoutScreen from './components/screens/workout-screen/WorkoutScreen'
 import ProgressScreen from './components/screens/progress-screen/ProgressScreen'
+import SettingsScreen from './components/screens/settings-screen/SettingsScreen'
 
+import { ScreenNav } from './components/organisms'
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
-import { ScreenNav } from './components/organisms'
 
 const Stack = createNativeStackNavigator()
 
 const App = (): JSX.Element => {
   return (
-    <View style={{ width: '100%', height: '100%' }}>
+    <View style={{ flex: 1 }}>
       <NavigationContainer>
         <Stack.Navigator initialRouteName='Progress'>
           <Stack.Screen
@@ -21,6 +22,11 @@ const App = (): JSX.Element => {
           <Stack.Screen
             name='Workouts'
             component={WorkoutScreen}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen
+            name='Settings'
+            component={SettingsScreen}
             options={{ headerShown: false }}
           />
         </Stack.Navigator>
